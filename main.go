06159b899d2196e2f96f8e3b4907f04f11a5c89b
c-danil0o <danilo.cvijetic10@protonmail.ch>
2023-01-26 19:08:12 +0100

    fixed config file

--- main.go
+++ main.go
@@ -8,26 +8,56 @@ func main() {
 
 	result := mt.ReadFile("config.json")
 
-	kapacitet := result["capacity"]
-	brSeg := result["num_of_segments"]
-	treshold := result["trashold"]
-
+	kapacitet := result["memtable_capacity"]
+	brSeg := result["memtable_num_of_segments"]
+	treshold := result["memtable_threshold"]
 	mt.Init(kapacitet, brSeg, treshold)
-	mt.Active.Add(mt.Element{Key: []byte("abcdea"), Value: []byte("danilo")})
-	mt.Active.Add(mt.Element{Key: []byte("abcdeb"), Value: []byte("danilo")})
-	mt.Active.Add(mt.Element{Key: []byte("abcdec"), Value: []byte("danilo")})
-	mt.Active.Add(mt.Element{Key: []byte("abcded"), Value: []byte("danilo")})
-	mt.Active.Add(mt.Element{Key: []byte("abcdee"), Value: []byte("danilo")})
-	mt.Active.Add(mt.Element{Key: []byte("abcdef"), Value: []byte("danilo")})
-	mt.Active.Add(mt.Element{Key: []byte("abcdeg"), Value: []byte("danilo")})
-	mt.Active.Add(mt.Element{Key: []byte("abcdeh"), Value: []byte("danilo")})
-	mt.Active.Add(mt.Element{Key: []byte("abcdei"), Value: []byte("danilo")})
-	mt.Active.Add(mt.Element{Key: []byte("abcdej"), Value: []byte("danilo")})
-	mt.Active.Add(mt.Element{Key: []byte("abcdek"), Value: []byte("danilo")})
-	mt.Active.Add(mt.Element{Key: []byte("abcdel"), Value: []byte("danilo")})
-	mt.Active.Add(mt.Element{Key: []byte("abcdem"), Value: []byte("danilo")})
-	mt.Active.Add(mt.Element{Key: []byte("abcden"), Value: []byte("danilo")})
-	mt.Active.Add(mt.Element{Key: []byte("abcdeo"), Value: []byte("danilo")})
-	mt.Active.Print()
 	mt.CheckThreshold()
+	mt.Active.Add(mt.Element{Key: []byte("abcdea"), Value: []byte("danilo")})
+	mt.CheckThreshold()
+
+	mt.Active.Add(mt.Element{Key: []byte("abcdeb"), Value: []byte("danilo")})
+	mt.CheckThreshold()
+
+	mt.Active.Add(mt.Element{Key: []byte("abcdec"), Value: []byte("danilo")})
+	mt.CheckThreshold()
+
+	mt.Active.Add(mt.Element{Key: []byte("abcded"), Value: []byte("danilo")})
+	mt.CheckThreshold()
+
+	mt.Active.Add(mt.Element{Key: []byte("abcdee"), Value: []byte("danilo")})
+	mt.CheckThreshold()
+
+	mt.Active.Add(mt.Element{Key: []byte("abcdef"), Value: []byte("danilo")})
+	mt.CheckThreshold()
+
+	mt.Active.Add(mt.Element{Key: []byte("abcdeg"), Value: []byte("danilo")})
+	mt.CheckThreshold()
+
+	mt.Active.Add(mt.Element{Key: []byte("abcdeh"), Value: []byte("danilo")})
+	mt.CheckThreshold()
+
+	mt.Active.Add(mt.Element{Key: []byte("abcdei"), Value: []byte("danilo")})
+	mt.CheckThreshold()
+
+	mt.Active.Add(mt.Element{Key: []byte("abcdej"), Value: []byte("danilo")})
+	mt.CheckThreshold()
+
+	mt.Active.Add(mt.Element{Key: []byte("abcdek"), Value: []byte("danilo")})
+	mt.CheckThreshold()
+
+	mt.Active.Add(mt.Element{Key: []byte("abcdel"), Value: []byte("danilo")})
+	mt.CheckThreshold()
+
+	mt.Active.Add(mt.Element{Key: []byte("abcdem"), Value: []byte("danilo")})
+	mt.CheckThreshold()
+
+	mt.Active.Add(mt.Element{Key: []byte("abcden"), Value: []byte("danilo")})
+	mt.CheckThreshold()
+
+	mt.Active.Add(mt.Element{Key: []byte("abcdeo"), Value: []byte("danilo")})
+	mt.CheckThreshold()
+
+	mt.Active.Print()
+	//mt.CheckThreshold()
 }
